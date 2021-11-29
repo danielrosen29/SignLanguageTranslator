6290415331a4d24226d5a0104c9414e6532b88a8
Elijah <elijahmtcastle@gmail.com>
2021-11-29 15:43:10 -0700

    load network

--- main.py
+++ main.py
@@ -1,7 +1,29 @@
 import cv2
 import numpy as np
-cap = cv2.VideoCapture(0)
+import tensorflow as tf
+import tensorflow as tf
+from tensorflow.keras import Sequential
+from tensorflow.keras.layers import Input, Dense, Dropout, Flatten, Conv2D, MaxPool2D
 
+NNPATH = "./checkpoint/checkpoint.ckpt"#path to NN
+
+
+def loadNN():#load the model
+    layers =  [Input(shape=(28,28,1),name='shape'),#make layers
+        Conv2D(16,3,padding="same",activation="relu"),
+        MaxPool2D(),
+        Conv2D(32,3,padding="same",activation="relu"),
+        MaxPool2D(),
+        Flatten(),
+        Dense(units=26, activation="softmax",name="out")]
+    
+    cnn_model = Sequential(layers)
+    cnn_model.load_weights(NNPATH)#load the data
+    cnn_model.summary()#print a summary of the model
+    
+    return cnn_model
+
+cap = cv2.VideoCapture(0)
 while True:
     ret, frame = cap.read()
     if not ret:
